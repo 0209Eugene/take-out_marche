e774389bf86ae49da25a3c55784fbccb82c83a60
Yuto Miura <veda.ym0209@gmail.com>
2020-05-12 00:07:10 +0900

    add styles

--- src/components/menu-item/menu-item.component.tsx
+++ src/components/menu-item/menu-item.component.tsx
@@ -10,13 +10,14 @@ type MenuItemProps = {
 
 const MenuItem: React.FC<MenuItemProps> = ({ key, title, imageUrl, size }) => {
   return (
-    <div
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-      className={`${size} menu-item`} key={key}>
+    <div className={`${size} menu-item`} key={key}>
+      <div className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className='subtitle'>Chicken Curry</span>
     </div>
     </div>
